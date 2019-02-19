c8685e11828e199d84fbaf90dd4aa216e3c24658
yuli hunsicker <yhunsicker@gmail.com>
2019-02-19 12:17:50 +0100

    Add Task Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'task', to: 'pages#task', as: :task
 end
