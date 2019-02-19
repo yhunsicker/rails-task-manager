68807248f7af15ef8563c18093c6f56400d6380c
yuli hunsicker <yhunsicker@gmail.com>
2019-02-19 19:51:08 +0100

    rails task manager challenge completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,43 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    id = params[:id]
+    @task = Task.find(id)
+  end
+
+  def update
+    id = params[:id]
+    @task = Task.find(id)
+    @task.update(task_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    id = params[:id]
+    @task = Task.find(id)
+    @task.destroy
+
+    redirect_to tasks_path
+  end
 end
 
+private
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
